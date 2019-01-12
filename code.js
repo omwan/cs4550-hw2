44bd8c36e9b1f43d8a4ae973147faddb48650890
Olivia Wan <wan.o@husky.neu.edu>
2019-01-12 15:06:06 -0500

    removing unneeded boolean flags

--- code.js
+++ code.js
@@ -21,9 +21,7 @@ var calculate = function(operator, number1, number2) {
         var command = null;
         var number1 = input.value;
         var number2 = null;
-        var decimalSelected = false;
         var operation = null;
-        var operationSelected = false;
 
         button.addEventListener("click", function() {
             command = button.innerHTML;
@@ -33,43 +31,33 @@ var calculate = function(operator, number1, number2) {
                     input.value = 0;
                     number1 = 0;
                     number2 = null;
-                    decimalSelected = false;
-                    opeation = null;
-                    operationSelected = false;
+                    operation = null;
                     break;
                 case "+/=":
+                case "-":
+                case "×":
+                case "÷":
                     if (operation != null) {
                         number2 = input.value;
                         var output = calculate(operation, number1, number2);
                         input.value = output;
                         number1 = output;
                         number2 = null;
-                        operation = null;
                     } else {
                         number1 = input.value;
                         operation = command;
                     }
-                    operationSelected = true;
-                    break;
-                case "-":
-                case "×":
-                case "÷":
-                    number1 = input.value;
-                    operation = command;
-                    operationSelected = true;
                     break;
                 case ".":
-                    if (!decimalSelected) {
+                    if (!input.value.includes(".")) {
                         input.value += command;
-                        decimalSelected = true;
                     }
                     break;
                 default:
-                    if (input.value != "0" && !operationSelected) {
+                    if (input.value != "0" && operation == null) {
                         input.value += command;
                     } else {
                         input.value = command;
-                        operationSelected = false;
                     }
             }
         });
