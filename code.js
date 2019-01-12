261fc57c0e12ca43c0ee43e358002fc848bb6681
Olivia Wan <wan.o@husky.neu.edu>
2019-01-12 16:14:46 -0500

    start new operation if new number typed when showing output of previous

--- code.js
+++ code.js
@@ -22,6 +22,7 @@ var calculate = function(operator, number1, number2) {
         var number1 = input.value;
         var number2 = null;
         var operation = null;
+        var operationCompleted = false;
 
         button.addEventListener("click", function() {
             command = button.innerHTML;
@@ -37,12 +38,13 @@ var calculate = function(operator, number1, number2) {
                 case "-":
                 case "×":
                 case "÷":
-                    if (operation != null) {
+                    if (operation != null && number1 != null) {
                         number2 = input.value;
                         var output = calculate(operation, number1, number2);
                         input.value = output;
                         number1 = output;
                         number2 = null;
+                        operationCompleted = true;
                     } else {
                         number1 = input.value;
                         operation = command;
@@ -57,6 +59,11 @@ var calculate = function(operator, number1, number2) {
                     if (input.value != "0" && operation == null) {
                         input.value += command;
                     } else {
+                        //start new operation
+                        if (operationCompleted) {
+                            number1 = null;
+                            operationCompleted = false;
+                        }
                         input.value = command;
                     }
             }
